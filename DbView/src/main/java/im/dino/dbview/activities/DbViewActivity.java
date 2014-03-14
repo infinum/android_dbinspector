b17bed8de5938a7e77a1c727cbc05cb3ae2a5810
Dino Kovač <dino.kovac@infinum.hr>
2014-03-14 21:13:52 +0100

    Removed support v4 and v7 - supporting only 14+

--- DbView/src/main/java/im/dino/dbview/activities/DbViewActivity.java
+++ DbView/src/main/java/im/dino/dbview/activities/DbViewActivity.java
@@ -1,28 +1,33 @@
 package im.dino.dbview.activities;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
 
 import im.dino.dbview.R;
 import im.dino.dbview.fragments.DatabaseListFragment;
 
-public class DbViewActivity extends ActionBarActivity {
+public class DbViewActivity extends Activity {
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db_view);
 
         if (savedInstanceState == null) {
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
             ft.replace(R.id.container, new DatabaseListFragment()).commit();
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Override
     public boolean onNavigateUp() {
-        getSupportFragmentManager().popBackStack();
+
+        getFragmentManager().popBackStack();
 
         return true;
     }
