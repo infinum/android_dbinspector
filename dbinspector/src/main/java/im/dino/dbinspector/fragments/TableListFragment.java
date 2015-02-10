bbf1536b9cc2fa7f0ba5702334fc5a73f1210dc1
Prasham <prash2488@gmail.com>
2015-02-10 09:12:21 +0530

    Added version code

--- dbinspector/src/main/java/im/dino/dbinspector/fragments/TableListFragment.java
+++ dbinspector/src/main/java/im/dino/dbinspector/fragments/TableListFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,45 +21,46 @@ import java.util.List;
  * Created by dino on 24/02/14.
  */
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class TableListFragment extends ListFragment {
+public class TableListFragment
+		extends ListFragment {
 
-    private static final String KEY_DATABASE = "database_name";
+	private static final String KEY_DATABASE = "database_name";
 
-    private File database;
-    private AdapterView.OnItemClickListener tableClickListener = new AdapterView.OnItemClickListener() {
+	private File database;
+	private AdapterView.OnItemClickListener tableClickListener = new AdapterView.OnItemClickListener() {
 
-	    @Override
-	    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-		    FragmentManager fm = getActivity().getSupportFragmentManager();
+			FragmentManager fm = getActivity().getSupportFragmentManager();
 
-		    FragmentTransaction ft = fm.beginTransaction();
-		    ft.replace(R.id.dbinspector_container, TableFragment.newInstance(database, (String) getListAdapter().getItem(position)));
-		    ft.addToBackStack(null).commit();
+			FragmentTransaction ft = fm.beginTransaction();
+			ft.replace(R.id.dbinspector_container, TableFragment.newInstance(database, (String) getListAdapter().getItem(position)));
+			ft.addToBackStack(null).commit();
 
-		    fm.executePendingTransactions();
-	    }
-    };
+			fm.executePendingTransactions();
+		}
+	};
 
-    public static TableListFragment newInstance(File database) {
+	public static TableListFragment newInstance(File database) {
 
-	    Bundle args = new Bundle();
-	    args.putSerializable(KEY_DATABASE, database);
+		Bundle args = new Bundle();
+		args.putSerializable(KEY_DATABASE, database);
 
-	    TableListFragment tlf = new TableListFragment();
-	    tlf.setArguments(args);
+		TableListFragment tlf = new TableListFragment();
+		tlf.setArguments(args);
 
-	    return tlf;
-    }
+		return tlf;
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-	    if (getArguments() != null) {
-		    database = (File) getArguments().getSerializable(KEY_DATABASE);
-	    }
-    }
+		if (getArguments() != null) {
+			database = (File) getArguments().getSerializable(KEY_DATABASE);
+		}
+	}
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
@@ -66,10 +68,17 @@ public class TableListFragment extends ListFragment {
 
 		ActionBarActivity activity = (ActionBarActivity) getActivity();
 
-		activity.getSupportActionBar().setTitle(database.getName());
-		activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 		List<String> tableList = DatabaseHelper.getAllTables(database);
+		String version = DatabaseHelper.getVersion(database);
+		ActionBar bar = activity.getSupportActionBar();
+		if (bar != null) {
+
+			bar.setTitle(database.getName());
+			bar.setSubtitle("v" + version);
+			bar.setDisplayHomeAsUpEnabled(true);
+		}
+
 
 		ListAdapter adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, tableList);
 
--- dbinspector/src/main/java/im/dino/dbinspector/helpers/DatabaseHelper.java
+++ dbinspector/src/main/java/im/dino/dbinspector/helpers/DatabaseHelper.java
@@ -4,98 +4,113 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
-
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * Created by dino on 23/02/14.
  */
 public class DatabaseHelper {
 
-    public static final String LOGTAG = "DBINSPECTOR";
+	public static final String LOGTAG = "DBINSPECTOR";
 
-    public static final String COLUMN_CID = "cid";
+	public static final String COLUMN_CID = "cid";
 
-    public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_NAME = "name";
 
-    public static final String COLUMN_TYPE = "type";
+	public static final String COLUMN_TYPE = "type";
 
-    public static final String COLUMN_NOT_NULL = "not null";
+	public static final String COLUMN_NOT_NULL = "not null";
 
-    public static final String COLUMN_DEFAULT = "default value";
+	public static final String COLUMN_DEFAULT = "default value";
 
-    public static final String COLUMN_PRIMARY = "primary key";
+	public static final String COLUMN_PRIMARY = "primary key";
 
-    public static final String TABLE_LIST_QUERY
-        = "SELECT name FROM sqlite_master WHERE type='table'";
+	public static final String TABLE_LIST_QUERY = "SELECT name FROM sqlite_master WHERE type='table'";
 
-    public static final String PRAGMA_FORMAT = "PRAGMA table_info(%s)";
+	public static final String PRAGMA_FORMAT = "PRAGMA table_info(%s)";
 
-    public static String getSqliteDir(Context context) {
-        return context.getFilesDir().getParent() + File.separator + "databases" + File.separator;
-    }
+	public static List<File> getDatabaseList(Context context) {
+		List<File> databaseList = new ArrayList<>();
 
-    public static List<File> getDatabaseList(Context context) {
-        List<File> databaseList = new ArrayList<>();
+		File sqliteDir = new File(DatabaseHelper.getSqliteDir(context));
 
-        File sqliteDir = new File(DatabaseHelper.getSqliteDir(context));
+		FilenameFilter filenameFilter = new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String filename) {
+				return filename.endsWith(".sql") || filename.endsWith(".sqlite") || filename.endsWith(".db") || filename.endsWith(".cblite");
+			}
+		};
 
-        FilenameFilter filenameFilter = new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String filename) {
-                return filename.endsWith(".sql")
-                    || filename.endsWith(".sqlite")
-                    || filename.endsWith(".db")
-                    || filename.endsWith(".cblite");
-            }
-        };
+		// look for standard sqlite databases in the databases dir
+		File[] sqliteFiles = sqliteDir.listFiles(filenameFilter);
+		if (sqliteFiles != null) {
+			databaseList.addAll(Arrays.asList(sqliteFiles));
+		} else {
+			Log.d(LOGTAG, "Database file list is null!");
+		}
 
-        // look for standard sqlite databases in the databases dir
-        File[] sqliteFiles = sqliteDir.listFiles(filenameFilter);
-        if (sqliteFiles != null) {
-            databaseList.addAll(Arrays.asList(sqliteFiles));
-        } else {
-            Log.d(LOGTAG, "Database file list is null!");
-        }
+		// CouchBase Lite stores the databases in the app files dir
+		String[] cbliteFiles = context.fileList();
+		for (String filename : cbliteFiles) {
+			if (filenameFilter.accept(context.getFilesDir(), filename)) {
+				databaseList.add(new File(context.getFilesDir(), filename));
+			}
+		}
 
-        // CouchBase Lite stores the databases in the app files dir
-        String[] cbliteFiles = context.fileList();
-        for (String filename : cbliteFiles) {
-            if (filenameFilter.accept(context.getFilesDir(), filename)) {
-                databaseList.add(new File(context.getFilesDir(), filename));
-            }
-        }
+		return databaseList;
+	}
 
-        return databaseList;
-    }
+	public static String getSqliteDir(Context context) {
+		return context.getFilesDir().getParent() + File.separator + "databases" + File.separator;
+	}
 
-    public static List<String> getAllTables(File database) {
+	public static String getVersion(File database) {
+		CursorOperation<String> operation = new CursorOperation<String>(database) {
+			@Override
+			public Cursor provideCursor(SQLiteDatabase database) {
+				return database.rawQuery("PRAGMA user_version", null);
+			}
 
-        CursorOperation<List<String>> operation = new CursorOperation<List<String>>(database) {
-            @Override
-            public Cursor provideCursor(SQLiteDatabase database) {
-                return database.rawQuery(TABLE_LIST_QUERY, null);
-            }
+			@Override
+			public String provideResult(SQLiteDatabase database, Cursor cursor) {
+				String result = "";
+				if (cursor.moveToFirst()) {
+					result = cursor.getString(0);
+				}
+				return result;
+			}
+		};
+		return operation.execute();
 
-            @Override
-            public List<String> provideResult(SQLiteDatabase database, Cursor cursor) {
-                List<String> tableList = new ArrayList<>();
-                if (cursor.moveToFirst()) {
-                    while (!cursor.isAfterLast()) {
-                        tableList.add(cursor.getString(cursor.getColumnIndex(COLUMN_NAME)));
-                        cursor.moveToNext();
-                    }
-                }
-                return tableList;
-            }
-        };
 
-        return operation.execute();
-    }
+	}
+
+	public static List<String> getAllTables(File database) {
+
+		CursorOperation<List<String>> operation = new CursorOperation<List<String>>(database) {
+			@Override
+			public Cursor provideCursor(SQLiteDatabase database) {
+				return database.rawQuery(TABLE_LIST_QUERY, null);
+			}
+
+			@Override
+			public List<String> provideResult(SQLiteDatabase database, Cursor cursor) {
+				List<String> tableList = new ArrayList<>();
+				if (cursor.moveToFirst()) {
+					while (!cursor.isAfterLast()) {
+						tableList.add(cursor.getString(cursor.getColumnIndex(COLUMN_NAME)));
+						cursor.moveToNext();
+					}
+				}
+				return tableList;
+			}
+		};
+
+		return operation.execute();
+	}
 
 }
