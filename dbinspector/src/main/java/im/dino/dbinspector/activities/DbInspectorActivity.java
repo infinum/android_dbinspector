57d5db3f99cd853e17aa848619abf201ab49085f
Prasham <prash2488@gmail.com>
2015-02-16 09:37:20 +0530

    Re-Styling the code and uncommenting gradle for better merging.

--- dbinspector/src/main/java/im/dino/dbinspector/activities/DbInspectorActivity.java
+++ dbinspector/src/main/java/im/dino/dbinspector/activities/DbInspectorActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
+
 import im.dino.dbinspector.fragments.DatabaseListFragment;
 import im.dino.dbview.R;
 
@@ -18,28 +19,28 @@ public class DbInspectorActivity extends ActionBarActivity {
         setContentView(R.layout.dbinspector_activity);
 
         if (savedInstanceState == null) {
-	        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-	        ft.replace(R.id.dbinspector_container, new DatabaseListFragment()).commit();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.dbinspector_container, new DatabaseListFragment()).commit();
         }
     }
 
     @Override
     public void onBackPressed() {
-	    if (getFragmentManager().getBackStackEntryCount() > 0) {
-		    getFragmentManager().popBackStack();
-	    } else {
-		    super.onBackPressed();
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
         }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-		    // Respond to the action bar's Up/Home button
-		    case android.R.id.home:
-			    onBackPressed();
-			    return true;
-	    }
-	    return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- dbinspector/src/main/java/im/dino/dbview/CopyDbIntentService.java
+++ dbinspector/src/main/java/im/dino/dbview/CopyDbIntentService.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import im.dino.dbinspector.helpers.DatabaseHelper;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+
+import im.dino.dbinspector.helpers.DatabaseHelper;
 
 
 /**
@@ -21,73 +23,71 @@ import java.io.OutputStream;
  * TODO: Customize class - update intent actions and extra parameters.
  */
 public class CopyDbIntentService
-		extends IntentService {
+        extends IntentService {
 
-	public static final String EXTRA_PATH = "PATH";
+    public static final String EXTRA_PATH = "PATH";
 
-	public CopyDbIntentService() {
-		super("CopyDbIntentService");
-	}
+    public CopyDbIntentService() {
+        super("CopyDbIntentService");
+    }
 
-	/**
-	 * Start service to copy the database. It will be copied to the app's director
-	 *
-	 * @param context
-	 * 		: Context to start service
-	 * @param database
-	 */
-	public static void startService(Context context, File database) {
-		Bundle bundle = new Bundle();
-		bundle.putSerializable("File", database);
-		Intent intent = new Intent(context, CopyDbIntentService.class);
-		intent.putExtras(bundle);
-		context.startService(intent);
-	}
+    /**
+     * Start service to copy the database. It will be copied to the app's director
+     *
+     * @param context : Context to start service
+     */
+    public static void startService(Context context, File database) {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("File", database);
+        Intent intent = new Intent(context, CopyDbIntentService.class);
+        intent.putExtras(bundle);
+        context.startService(intent);
+    }
 
-	@Override
-	protected void onHandleIntent(Intent intent) {
-		if (intent != null) {
-			File database = (File) intent.getSerializableExtra("File");
-			String directoryName = getString(getApplicationInfo().labelRes);
-			File appDirectory = new File(Environment.getExternalStorageDirectory().toString(), directoryName);
-			if (!appDirectory.exists() || !appDirectory.isDirectory()) {
-				appDirectory.mkdir();
-			}
-			String path = appDirectory.getAbsolutePath();
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            File database = (File) intent.getSerializableExtra("File");
+            String directoryName = getString(getApplicationInfo().labelRes);
+            File appDirectory = new File(Environment.getExternalStorageDirectory().toString(), directoryName);
+            if (!appDirectory.exists() || !appDirectory.isDirectory()) {
+                appDirectory.mkdir();
+            }
+            String path = appDirectory.getAbsolutePath();
 
-			File sqliteDir = new File(DatabaseHelper.getSqliteDir(this));
-			if (database != null) {
-				InputStream in = null;
-				OutputStream out = null;
-				try {
-					String databaseFileName = database.getName();
-					File opFile = new File(appDirectory, databaseFileName);
-					in = new FileInputStream(database);
-					out = new FileOutputStream(opFile);
+            File sqliteDir = new File(DatabaseHelper.getSqliteDir(this));
+            if (database != null) {
+                InputStream in = null;
+                OutputStream out = null;
+                try {
+                    String databaseFileName = database.getName();
+                    File opFile = new File(appDirectory, databaseFileName);
+                    in = new FileInputStream(database);
+                    out = new FileOutputStream(opFile);
 
-					byte[] buffer = new byte[1024];
-					int read;
-					while ((read = in.read(buffer)) != -1) {
-						out.write(buffer, 0, read);
-					}
-					in.close();
-					in = null;
+                    byte[] buffer = new byte[1024];
+                    int read;
+                    while ((read = in.read(buffer)) != -1) {
+                        out.write(buffer, 0, read);
+                    }
+                    in.close();
+                    in = null;
 
-					// write the output file
-					out.flush();
-					out.close();
-					out = null;
+                    // write the output file
+                    out.flush();
+                    out.close();
+                    out = null;
 
 
-				} catch (Exception e) {
-					Log.e("tag", e.getMessage());
-				}
+                } catch (Exception e) {
+                    Log.e("tag", e.getMessage());
+                }
 
-				LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent("DatabaseCopied"));
+                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent("DatabaseCopied"));
 
-			}
-		}
-	}
+            }
+        }
+    }
 
 
 }
