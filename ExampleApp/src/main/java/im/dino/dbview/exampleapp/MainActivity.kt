b057682ae09825a7f8833bfbf724941ccb94707e
Bojan Komljenović <bojan.komljenovic@infinum.hr>
2020-09-22 21:23:02 +0200

    Add example databases

--- ExampleApp/src/main/java/im/dino/dbview/exampleapp/MainActivity.kt
+++ ExampleApp/src/main/java/im/dino/dbview/exampleapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package im.dino.dbview.exampleapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import im.dino.dbinspector.DbInspector
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +17,33 @@ class MainActivity : AppCompatActivity() {
         show.setOnClickListener {
             DbInspector.launch(this)
         }
+
+        copy()
+    }
+
+    private fun copy() {
+        content.visibility = View.GONE
+        progressBar.visibility = View.VISIBLE
+
+        var databasesDir = this.filesDir.path
+        databasesDir = databasesDir.substring(0, databasesDir.lastIndexOf("/")) + "/databases"
+
+        listOf(
+            "articles.db",
+            "chinook.db",
+            "northwind.sqlite"
+        )
+            .filterNot { File(databasesDir, it).exists() }
+            .forEach { filename ->
+                assets.open("databases/$filename").use { inputStream ->
+                    FileOutputStream(File(databasesDir, filename))
+                        .use { outputStream ->
+                            outputStream.write(inputStream.readBytes())
+                        }
+                }
+            }
+
+        progressBar.visibility = View.GONE
+        content.visibility = View.VISIBLE
     }
 }
--- dbinspector/src/main/java/im/dino/dbinspector/extensions/Context.kt
+++ dbinspector/src/main/java/im/dino/dbinspector/extensions/Context.kt
@@ -2,7 +2,7 @@ package im.dino.dbinspector.extensions
 
 import android.content.Context
 
-fun Context.databaseDir(): String {
+internal fun Context.databaseDir(): String {
     val destPath = this.filesDir.path
     return destPath.substring(0, destPath.lastIndexOf("/")) + "/databases"
 }
