3d7778c6d68c4f172c6cc6d97df755d2d467ca12
Bojan <bojan.komljenovic@infinum.hr>
2020-12-23 22:16:50 +0100

    Add support for JPG, PNG and BMP BLOB preview.

--- dbinspector/src/main/kotlin/com/infinum/dbinspector/domain/schema/shared/models/ImageType.kt
+++ dbinspector/src/main/kotlin/com/infinum/dbinspector/domain/schema/shared/models/ImageType.kt
@@ -3,8 +3,6 @@ package com.infinum.dbinspector.domain.schema.shared.models
 internal enum class ImageType(val header: String) {
     UNSUPPORTED(""),
     JPG("FFD8FFE0"),
-    TIFF("49492A"),
-    GIF("474946"),
     BMP("424D"),
     PNG("89504E470D0A1A0A");
 
