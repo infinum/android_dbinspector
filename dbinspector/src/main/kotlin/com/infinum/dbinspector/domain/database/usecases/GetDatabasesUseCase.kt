08a6a6f4b17309197ff1b87a3537b4b689f85b1e
Bojan <bojan.komljenovic@infinum.hr>
2020-12-31 01:50:50 +0100

    Prettify code

--- dbinspector/src/main/kotlin/com/infinum/dbinspector/domain/database/usecases/GetDatabasesUseCase.kt
+++ dbinspector/src/main/kotlin/com/infinum/dbinspector/domain/database/usecases/GetDatabasesUseCase.kt
@@ -18,20 +18,16 @@ internal class GetDatabasesUseCase(
         databaseRepository.getPage(input)
             .filter { descriptor -> input.argument?.let { descriptor.name.contains(it) } ?: true }
             .map {
-                // TODO: This entire map needs to be extracted and refactored
                 val connection = connectionRepository.open(ConnectionParameters(databasePath = it.absolutePath))
-
-                val descriptorWithVersion = it.copy(
-                    version = pragmaRepository.getUserVersion(
-                        PragmaParameters.Version(
-                            databasePath = it.absolutePath,
-                            database = connection.database,
-                            statement = Statements.Pragma.userVersion()
-                        )
-                    ).cells.first().text.orEmpty()
-                )
+                val version = pragmaRepository.getUserVersion(
+                    PragmaParameters.Version(
+                        databasePath = it.absolutePath,
+                        database = connection.database,
+                        statement = Statements.Pragma.userVersion()
+                    )
+                ).cells.firstOrNull()?.text.orEmpty()
                 connectionRepository.close(ConnectionParameters(databasePath = it.absolutePath))
 
-                descriptorWithVersion
+                it.copy(version = version)
             }
 }
