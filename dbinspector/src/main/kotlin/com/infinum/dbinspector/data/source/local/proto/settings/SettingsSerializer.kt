d8a189c9df0e41d7860169462f293fed3f772afb
Bojan <bojan.komljenovic@infinum.hr>
2021-03-11 09:56:14 +0100

    Update dependencies.

--- dbinspector/src/main/kotlin/com/infinum/dbinspector/data/source/local/proto/settings/SettingsSerializer.kt
+++ dbinspector/src/main/kotlin/com/infinum/dbinspector/data/source/local/proto/settings/SettingsSerializer.kt
@@ -6,20 +6,27 @@ import com.google.protobuf.InvalidProtocolBufferException
 import com.infinum.dbinspector.data.models.local.proto.output.SettingsEntity
 import java.io.InputStream
 import java.io.OutputStream
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 internal class SettingsSerializer : Serializer<SettingsEntity> {
 
     override val defaultValue: SettingsEntity =
         SettingsEntity.getDefaultInstance()
 
-    override fun readFrom(input: InputStream): SettingsEntity {
-        try {
-            return SettingsEntity.parseFrom(input)
-        } catch (exception: InvalidProtocolBufferException) {
-            throw CorruptionException("Cannot read proto.", exception)
+    override suspend fun readFrom(input: InputStream): SettingsEntity =
+        suspendCancellableCoroutine { continuation ->
+            try {
+                continuation.resume(SettingsEntity.parseFrom(input))
+            } catch (exception: InvalidProtocolBufferException) {
+                continuation.resumeWithException(CorruptionException("Cannot read proto.", exception))
+            }
         }
-    }
 
-    override fun writeTo(t: SettingsEntity, output: OutputStream) =
-        t.writeTo(output)
+    override suspend fun writeTo(t: SettingsEntity, output: OutputStream): Unit =
+        suspendCancellableCoroutine { continuation ->
+            t.writeTo(output)
+            continuation.resume(Unit)
+        }
 }
