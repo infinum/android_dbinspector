0df13890585eaee92130e9968f0e6436713c12cf
Bojan <bojan.komljenovic@infinum.hr>
2022-11-07 17:48:32 +0100

    Deploy new Angular build of remote views.

--- dbinspector/src/main/kotlin/com/infinum/dbinspector/data/sources/memory/connection/AndroidConnectionSource.kt
+++ dbinspector/src/main/kotlin/com/infinum/dbinspector/data/sources/memory/connection/AndroidConnectionSource.kt
@@ -3,25 +3,28 @@ package com.infinum.dbinspector.data.sources.memory.connection
 import android.database.sqlite.SQLiteDatabase
 import androidx.annotation.VisibleForTesting
 import com.infinum.dbinspector.data.Sources
+import java.util.Collections
 
 internal class AndroidConnectionSource : Sources.Memory.Connection {
 
+    private val connectionPool: HashMap<String, SQLiteDatabase> = hashMapOf()
     @VisibleForTesting
-    internal val connectionPool: HashMap<String, SQLiteDatabase> = hashMapOf()
+    internal val syncPool: MutableMap<String, SQLiteDatabase> = Collections.synchronizedMap(connectionPool)
 
     override suspend fun openConnection(path: String): SQLiteDatabase {
-        if (connectionPool.containsKey(path).not()) {
-            connectionPool[path] = SQLiteDatabase.openOrCreateDatabase(path, null)
+        if (syncPool.containsKey(path).not()) {
+            syncPool[path] = SQLiteDatabase.openOrCreateDatabase(path, null)
         }
-        return connectionPool[path] ?: SQLiteDatabase.openOrCreateDatabase(path, null)
+        return syncPool[path] ?: SQLiteDatabase.openOrCreateDatabase(path, null)
     }
 
     override suspend fun closeConnection(path: String) {
-        if (connectionPool.containsKey(path)) {
-            connectionPool[path]?.let {
+        if (syncPool.containsKey(path)) {
+            syncPool[path]?.let {
                 if (it.isOpen) {
-                    it.close()
-                    connectionPool.remove(path)
+                    // TODO: Fix this multithreading problem
+                    //it.close()
+                    syncPool.remove(path)
                 }
             }
         }
